b3878012802c96bbe63c6dbae8dcf0e57f77a257
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-07-15 21:16:02 +0800

    chore: use GM_ instead of localStorage

--- src/utils/create-local-storage.ts
+++ src/utils/create-local-storage.ts
@@ -2,10 +2,10 @@ import { createEffect } from 'solid-js'
 import { createStore } from 'solid-js/store'
 
 export function createLocalStore<T extends object>(name: string, init: T) {
-  const localState = localStorage.getItem(name)
-  const [state, setState] = createStore<T>(
-    localState ? JSON.parse(localState) : init
-  )
-  createEffect(() => localStorage.setItem(name, JSON.stringify(state)))
+  const value = GM_getValue(name, init)
+  const [state, setState] = createStore<T>(value)
+
+  createEffect(() => GM_setValue(name, state))
+
   return [state, setState] as const
 }
