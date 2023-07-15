39c008d95a0c2e02ea2bde7a727af902b90ee67f
crashmax-dev <crashmax.dev@gmail.com>
2023-07-15 08:59:27 +0000

    Deploying to gh-pages from @ crashmax-dev/tryrating-userscript@36c3fd0577b28ce4eb130e9cb32086fe1da83248 🚀

--- tryrating-userscript.meta.js
+++ tryrating-userscript.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        tryrating-userscript
-// @version     1.4.0
+// @version     1.4.1
 // @author      crashmax <userscript@crashmax.ru>
 // @license     MIT
 // @homepage    https://crashmax-dev.github.io/tryrating-userscript/
