46521ff474afb91443fb77751e1fe0abfaacafd2
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-07-15 17:51:25 +0800

    fix: task fields observer

--- src/features/observe-elements.ts
+++ src/features/observe-elements.ts
@@ -1,4 +1,5 @@
 import { observeElement, waitElement } from '@zero-dependency/dom'
+import { setInterval } from 'worker-timers'
 import { logger } from '../utils/logger.js'
 import { autoCloseModal } from './auto-close-modal.js'
 import { taskFieldsObserver } from './task-fields-observer.js'
@@ -14,9 +15,8 @@ export async function setObserverApp(): Promise<void> {
   observeElement(appRoot, () => autoCloseModal())
 
   waitElement(taskFieldsObserver.targetSelector)
-    .then((el) => {
-      taskFieldsObserver.observe()
-      observeElement(el, () => taskFieldsObserver.observe())
+    .then(() => {
+      setInterval(() => taskFieldsObserver.observe(), 5000)
     })
     .finally(() => logger.info('Initialized task fields observer'))
 }
